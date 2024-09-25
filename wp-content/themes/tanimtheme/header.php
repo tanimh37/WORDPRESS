b741139f60d5231aac650c478ad8b5d99f941330
Tanim Hasan <tanimh37@gmail.com>
2024-09-25 13:07:49 +0600

    9/25

---
+++ wp-content/themes/tanimtheme/header.php
@@ -0,0 +1,35 @@
+<header>
+	<div class="wrap-header zerogrid">
+		<div id="logo"><a href="<?php echo home_url() ?>"><img src="<?php echo get_template_directory_uri() ?>/images/logo.png"/></a></div>
+		
+		<div id="search">
+			<div class="button-search"></div>
+			<input type="text" value="Search..." onfocus="if (this.value == &#39;Search...&#39;) {this.value = &#39;&#39;;}" onblur="if (this.value == &#39;&#39;) {this.value = &#39;Search...&#39;;}">
+		</div>
+	</div>
+</header>
+
+<nav>
+	<div class="wrap-nav zerogrid">
+		<div class="menu">
+			<ul>
+				<li><a href="index.html">Home</a></li>
+				<li class="current"><a href="blog.html">Blog</a></li>
+				<li><a href="gallery.html">Gallery</a></li>
+				<li><a href="single.html">About</a></li>
+				<li><a href="contact.html">Contact</a></li>
+			</ul>
+		</div>
+		
+		<div class="minimenu"><div>MENU</div>
+			<select onchange="location=this.value">
+				<option></option>
+				<option value="index.html">Home</option>
+				<option value="blog.html">Blog</option>
+				<option value="gallery.html">Gallery</option>
+				<option value="single.html">About</option>
+				<option value="contact.html">Contact</option>
+			</select>
+		</div>		
+	</div>
+</nav>
